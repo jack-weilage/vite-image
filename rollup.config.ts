ffffbe16f9f1c670a490f781cd31b5681ab5b7e0
Jack Weilage <jack@weilage.dev>
2022-09-24 21:06:25 -0700

    Use TypeScript config, properly handle deps

---
+++ rollup.config.ts
@@ -0,0 +1,25 @@
+import type { RollupOptions } from 'rollup'
+
+import pkg from './package.json'
+import { builtinModules } from 'module'
+
+import typescript from '@rollup/plugin-typescript'
+
+
+export default {
+    external: [ ...Object.keys(pkg.dependencies), ...builtinModules ],
+    input: 'src/index.ts',
+    output: [
+        {
+            file: 'dist/index.cjs',
+            format: 'cjs',
+            exports: 'auto'
+        },
+        {
+            file: 'dist/index.mjs',
+            format: 'esm',
+            exports: 'auto'
+        }
+    ],
+    plugins: [ typescript() ]
+} as RollupOptions
