00c1cbc0f3d652243e89d55a744b3e120e3a48e4
Jack Weilage <jack@weilage.dev>
2022-11-02 11:03:19 -0700

    Remove unnecessary tests

--- tests/import.test.ts
+++ tests/import.test.ts
@@ -17,32 +17,10 @@ describe('imports', () => {
         // Even fancier. An import with searchparams and hash
         [ './images/dog.jpg?foo=bar#baz-qux', '/assets/dog.1b15ce03.jpg?foo=bar#baz-qux' ]
     ])('does not affect default imports', async (input, output) => expect(await test(window, input)).toBe(output))
-    it.each([
-        [ './images/dog.jpg?width=600,1200&height=800,1600', [
-            [ 600, 800 ],
-            [ 600, 1600 ],
-            [ 1200, 800 ],
-            [ 1200, 1600 ]
-        ] ],
-        [ './images/dog.jpg?width=20,400,30&height=50', [
-            [ 20, 50 ],
-            [ 400, 50 ],
-            [ 30, 50 ]
-        ] ],
-        [ './images/dog.jpg?width=500,800,1000', [
-            [ 500, 375 ],
-            [ 800, 600 ],
-            [ 1000, 750 ]
-        ] ],
-        [ './images/dog.jpg?height=200,900,1000', [
-            [ 267, 200 ],
-            [ 1200, 900 ],
-            [ 1333, 1000 ]
-        ] ]
-    ])('affects custom imports', async (input, output) => {
-        const data = test(window, input)
-            .then(images => images.map(img => [ img.width, img.height ]))
+    
+    it('affects custom imports', async () => {
+        const data = test(window, './images/dog.jpg?width=600,800,1200')
         
-        expect(await data).toEqual(output)
+        expect(await data).toHaveLength(3)
     })
 })
