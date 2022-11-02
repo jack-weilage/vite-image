57f7553bf0352822926042bb1b38bafb619e899a
Jack Weilage <jack@weilage.dev>
2022-11-02 15:29:29 -0700

    Scaffold more testing

---
+++ tests/config.test.ts
@@ -0,0 +1,12 @@
+import { Window } from 'happy-dom'
+import { beforeEach, it, describe } from 'vitest'
+import { test } from './utils'
+
+let window: Window
+beforeEach(() => { window = new Window() })
+
+describe('config', () => {
+    it.todo('applies a default config')
+    it.todo('applies a custom config')
+    it.todo('actually uses a config')
+})
---
+++ tests/transform.test.ts
@@ -0,0 +1,25 @@
+//TODO: Use a screenshot to test the desired image.
+import { Window } from 'happy-dom'
+import { beforeEach, describe, it } from 'vitest'
+import { test } from './utils'
+
+let window: Window
+beforeEach(() => { window = new Window() })
+
+describe('resize', () => {
+    it.todo('applies the transform')
+    it.todo('applies the transform with multiple values')
+    it.todo('applies the transform with mixed values')
+})
+describe('format', () => {
+    it.todo('applies the transform')
+    it.todo('applies the transform with multiple values')
+})
+describe('blur', () => {
+    it.todo('applies the transform')
+    it.todo('applies the transform with multiple values')
+
+})
+describe('combined', () => {
+    it.todo('applies multiple transforms')
+})
