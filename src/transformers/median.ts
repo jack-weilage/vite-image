fa0ff0991bc9f5995391a13f525fdf0aa02f51d9
Jack Weilage <jack@weilage.dev>
2022-11-06 21:42:14 -0800

    Clean up some bugs with a couple transformers

--- src/transformers/median.ts
+++ src/transformers/median.ts
@@ -2,6 +2,6 @@ import { Transformer } from '../../types'
 
 export default {
     name: 'median',
-    matcher: (config) => config['median'] === true || typeof config['sharpen'] === 'number',
-    transform: (img, config) => img.median(config['median'] === true ? undefined : config['median'])
+    matcher: (config) => config['median'] === true || typeof config['median'] === 'number',
+    transform: (img, config) => img.median(config['median'] === true ? undefined : Math.min(Math.max(config['median'] as number, 1), 1000))
 } as Transformer
--- src/transformers/sharpen.ts
+++ src/transformers/sharpen.ts
@@ -3,5 +3,5 @@ import { Transformer } from '../../types'
 export default {
     name: 'sharpen',
     matcher: (config) => config['sharpen'] === true || typeof config['sharpen'] === 'number',
-    transform: (img, config) => img.sharpen(config['sharpen'] === true ? undefined : { sigma: config['sharpen'] as number })
+    transform: (img, config) => img.sharpen(config['sharpen'] === true ? undefined : { sigma: Math.min(Math.max(config['sharpen'] as number, 0.01), 10000) })
 } as Transformer
