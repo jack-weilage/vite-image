0de5b043a49067894dfc8f5beae86baa2aa3d052
Jack Weilage <jack@weilage.dev>
2022-11-07 11:47:13 -0800

    Update some nasty matchers

--- src/transformers/modulate.ts
+++ src/transformers/modulate.ts
@@ -1,18 +1,20 @@
-import { Transformer } from '../../types'
+import type { Transformer } from '../../types'
 
 export default {
     name: 'modulate',
-    // Ugly ugly ugly. All options are either undefined or number, but at least one option must be number.
-    matcher: (config) => 
-       (typeof config['brightness'] === 'undefined' || typeof config['brightness'] === 'number')
-    && (typeof config['saturation'] === 'undefined' || typeof config['saturation'] === 'number')
-    && (typeof config['hue'] === 'undefined'        || typeof config['hue'] === 'number')
-    && (typeof config['lightness'] === 'undefined'  || typeof config['lightness'] === 'number')
-    && (typeof (config['brightness'] ?? config['saturation'] ?? config['hue'] ?? config['lightness']) === 'number'),
+    // Surprisingly simple. 
+    // First, make sure that at least _one_ of the options is a number.
+    // Then make sure that _every_ option is either a number or undefined.
+    // This will skip checking _every_ option if a single option isn't valid.
+    matcher: (config) => typeof (config['brightness'] ?? config['saturation'] ?? config['hue'] ?? config['lightness']) === 'number'
+        && (typeof config['brightness'] === 'number' || typeof config['brightness'] === 'undefined')
+        && (typeof config['saturation'] === 'number' || typeof config['saturation'] === 'undefined')
+        && (typeof config['hue']        === 'number' || typeof config['hue']        === 'undefined')
+        && (typeof config['lightness']  === 'number' || typeof config['lightness']  === 'undefined'),
     transform: (img, config) => img.modulate({
         ...(typeof config['brightness'] === 'undefined' ? {  }: { brightness: config['brightness'] }),
         ...(typeof config['saturation'] === 'undefined' ? {  }: { saturation: config['saturation'] }),
-        ...(typeof config['hue'] === 'undefined' ? {  }: { hue: config['hue'] }),
-        ...(typeof config['lightness'] === 'undefined' ? {  }: { lightness: config['lightness'] }),
+        ...(typeof config['hue']        === 'undefined' ? {  }: { hue: config['hue'] }),
+        ...(typeof config['lightness']  === 'undefined' ? {  }: { lightness: config['lightness'] }),
     })
 } as Transformer
--- src/transformers/resize.ts
+++ src/transformers/resize.ts
@@ -7,22 +7,15 @@ const resize = (img: Sharp, width: number, height: number) =>
 
 export default {
     name: 'resize',
-    matcher: (config) => {
-        //TODO: This matcher can likely be replaced with the shorter one in `modulate`
-        const { width, height } = config
-
-        const width_valid = typeof width === 'number' && !isNaN(width)
-        const height_valid = typeof height === 'number' && !isNaN(height)
-
-        return width_valid && height_valid ||       // Both width and height are supplied
-            width_valid && height === undefined ||  // Only width is supplied
-            width === undefined && height_valid     // Only height is supplied
-    },
+    // See modulate.ts
+    matcher: (config) => typeof (config['width'] ?? config['height']) === 'number'
+        && (typeof config['width']  === 'number' || typeof config['width']  === 'undefined') 
+        && (typeof config['height'] === 'number' || typeof config['height'] === 'undefined'),
     transform: (img, config, metadata) => {
         const { width, height } = config
 
         // Check this first as we don't need metadata.
-        if (width !== undefined && height !== undefined)
+        if (typeof width !== 'undefined' && typeof height !== 'undefined')
             return resize(img, width, height)
 
         // Everything after this will need metadata width/height
@@ -30,10 +23,10 @@ export default {
             return
         
 
-        if (width && height === undefined)
+        if (width && typeof height === 'undefined')
             return resize(img, width, width / metadata.width * metadata.height)
 
-        if (width === undefined && height)
+        if (typeof width === 'undefined' && height)
             return resize(img, height / metadata.height * metadata.width, height)
             
         throw new Error('This shouldn\'t happen. How did you get here?')
