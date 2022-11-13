75792edfaf4419261267b2a3c6401ba27241e32e
Jack Weilage <jack@weilage.dev>
2022-11-13 14:01:10 -0800

    Use rollup to bundle TypeScript definitions

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,22 +1,33 @@
 import type { RollupOptions } from 'rollup'
 
 import esbuild from 'rollup-plugin-esbuild'
+import dts from 'rollup-plugin-dts'
 import { builtinModules } from 'module'
 
-export default {
-    external: [ '@rollup/pluginutils', 'magic-string', 'sharp', ...builtinModules ],
-    input: 'src/index.ts',
-    output: [
-        {
-            file: 'dist/index.cjs',
-            format: 'cjs',
-            exports: 'auto'
+export default [
+    {
+        external: [ '@rollup/pluginutils', 'magic-string', 'sharp', ...builtinModules ],
+        input: 'src/index.ts',
+        output: [
+            {
+                file: 'dist/index.cjs',
+                format: 'cjs',
+                exports: 'auto'
+            },
+            {
+                file: 'dist/index.mjs',
+                format: 'esm',
+                exports: 'auto'
+            }
+        ],
+        plugins: [ esbuild({ minify: true }) ]
+    },
+    {
+        input: 'src/index.ts',
+        output: {
+            file: 'dist/index.d.ts',
+            format: 'esm'
         },
-        {
-            file: 'dist/index.mjs',
-            format: 'esm',
-            exports: 'auto'
-        }
-    ],
-    plugins: [ esbuild({ minify: true }) ]
-} as RollupOptions
+        plugins: [ dts() ]
+    }
+] as RollupOptions
