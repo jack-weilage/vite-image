533b448cfcab2a4bb7bed8123b01dad8246bdd25
Jack Weilage <jack@weilage.dev>
2022-11-14 10:53:53 -0800

    Test config.

--- tests/config.test.ts
+++ tests/config.test.ts
@@ -1,12 +1,38 @@
-import { Window } from 'happy-dom'
-import { beforeEach, it, describe } from 'vitest'
-import { test } from './utils'
+import type { PluginConfig } from '../types'
 
-let window: Window
-beforeEach(() => { window = new Window() })
+import { parse_config } from '../src/utils'
+import { DEFAULT_CONFIG } from '../src/constants'
+import { it, expect } from 'vitest'
 
-describe('config', () => {
-    it.todo('applies a default config')
-    it.todo('applies a custom config')
-    it.todo('actually uses a config')
+it.each([
+    { test: true },
+    { include: 10 },
+    { transformers: [ 'foo', 'bar' ] }
+])('does not apply invalid config: %s', async (input) => {
+    // This seems to be the best way to match an AggregateError.
+    try {
+        //@ts-expect-error: The config _should_ be invalid here.
+        parse_config(input, DEFAULT_CONFIG)
+    } catch (error) {
+        if (!(error instanceof AggregateError))
+            throw error
+        // console.log((error as AggregateError).errors)
+        expect(error.errors).toMatchSnapshot()
+    }
+})
+
+it.each([
+    undefined,
+    {  },
+    { include: '**/*' },
+    { include: '**/*', exclude: '**/*.jpeg' },
+    { 
+        transformers: [{
+            name: 'noop',
+            matcher: () => true,
+            transform: (img: any) => img
+        }]
+    }
+])('affects custom imports: %s', async (input) => {
+    expect(parse_config(input as Partial<PluginConfig>, DEFAULT_CONFIG)).toMatchSnapshot()
 })
