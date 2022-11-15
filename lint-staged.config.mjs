6f08ccda89faa6112ce4532906d2a7439a73a47b
Jack Weilage <jack@weilage.dev>
2022-11-15 12:32:01 -0800

    Add tsconfig options to `lint-staged`.

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 export default {
     '**/*.ts': (filenames) => filenames
-        .map(filename => `tsc ${filename} --noEmit`)
+        .map(filename => `tsc ${filename} --target es2022 --noErrorTruncation --skipLibCheck --importsNotUsedAsValues error --noEmit --module esnext --moduleResolution node --strict`)
         .concat([ `pnpm test -- related ${filenames.join(' ')}` ])
 }
