b87225fb1a8a2ce649aff17bf8a9d509ec89116c
Jack Weilage <jack@weilage.dev>
2022-11-15 10:15:05 -0800

    Rotate works now!

--- src/transformers/rotate.ts
+++ src/transformers/rotate.ts
@@ -1,6 +1,5 @@
 import type { Transformer } from '../../types'
 
-//BUG: This doesn't seem to work with the automatic EXIF rotate.
 export default {
     name: 'rotate',
     matcher: (config) => typeof config['rotate'] === 'number',
