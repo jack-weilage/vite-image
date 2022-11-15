55104b6a7ce4b897a0153070f47999a6ce7eb60b
Jack Weilage <jack@weilage.dev>
2022-11-15 13:05:17 -0800

    Build before testing (oops).

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,7 @@
 export default {
-    '**/*.ts': (filenames) => filenames
-        .map(filename => `tsc ${filename} --target es2022 --noErrorTruncation --skipLibCheck --importsNotUsedAsValues error --noEmit --module esnext --moduleResolution node --strict`)
-        .concat([ `pnpm test -- related ${filenames.join(' ')}` ])
+    '**/*.ts': (filenames) => [
+        `pnpm build`,
+        ...filenames.map(filename => `tsc ${filename} --target es2022 --noErrorTruncation --skipLibCheck --importsNotUsedAsValues error --noEmit --module esnext --moduleResolution node --strict`),
+        `pnpm test -- related ${filenames.join(' ')}`
+    ]
 }
