06e6797df25aacc594ee011a81e79a7bb9d9c358
Jack Weilage <jack@weilage.dev>
2022-11-16 14:26:28 -0800

    Missed a key from `tsconfig.json`.

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,7 @@
 export default {
     '**/*.ts': (filenames) => [
         `pnpm build`,
-        ...filenames.map(filename => `tsc ${filename} --target es2022 --noErrorTruncation --skipLibCheck --importsNotUsedAsValues error --noEmit --module esnext --moduleResolution node --strict`),
+        ...filenames.map(filename => `tsc ${filename} --target es2022 --noErrorTruncation --skipLibCheck --importsNotUsedAsValues error --noEmit --module esnext --moduleResolution node --allowSyntheticDefaultImports --strict`),
         `pnpm test -- related ${filenames.join(' ')}`
     ]
 }
