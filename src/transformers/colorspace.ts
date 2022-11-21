06ce66b69503e3a7cdad8fd0d053791f2a0af12f
Jack Weilage <jack@weilage.dev>
2022-11-21 12:36:41 -0800

    Add proper names for transformers.

--- src/transformers/colorspace.ts
+++ src/transformers/colorspace.ts
@@ -4,7 +4,7 @@ import type { ColorspaceEnum, Transformer } from '../../types'
 import { INPUT_COLORSPACES } from '../constants'
 
 export default {
-    name: 'threshold',
+    name: 'colorspace',
     matcher: (config) => INPUT_COLORSPACES.includes(config['colorspace'] as keyof typeof ColorspaceEnum),
     //BUG: `@types/sharp` thinks that `toColorspace` should take a string, but doesn't narrow any further than that.
     transform: (img, config) => img.toColorspace(config['colorspace'] as string)
--- src/transformers/grayscale.ts
+++ src/transformers/grayscale.ts
@@ -1,7 +1,7 @@
 import type { Transformer } from '../../types'
 
 export default {
-    name: 'threshold',
+    name: 'grayscale',
     matcher: (config) => typeof config['grayscale'] === 'boolean',
     transform: (img, config) => img.grayscale(config['grayscale'])
 } as Transformer<'grayscale'>
