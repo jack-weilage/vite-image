c50ba23415ddcdf1f7c31aa655dfc025e6f7d43e
Jack Weilage <jack@weilage.dev>
2022-11-21 09:58:03 -0800

    Perform type-checking + build in parallel.

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,7 @@
 export default {
     '**/*.ts': (filenames) => [
-        `pnpm build`,
-        ...filenames.map(filename => `tsc ${filename} --target es2022 --noErrorTruncation --skipLibCheck --importsNotUsedAsValues error --noEmit --module esnext --moduleResolution node --allowSyntheticDefaultImports --strict`),
+        `pnpm build & ${filenames
+            .map(filename => `tsc ${filename} --target es2022 --noErrorTruncation --skipLibCheck --importsNotUsedAsValues error --noEmit --module esnext --moduleResolution node --allowSyntheticDefaultImports --strict`).join(' & ')}`,
         `pnpm test -- related ${filenames.join(' ')}`
     ]
 }
