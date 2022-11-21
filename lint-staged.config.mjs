ef47d6da5d251696d71b444a9e2358b4c81bc1cb
Jack Weilage <jack@weilage.dev>
2022-11-21 10:41:03 -0800

    Run typechecks in parallel (much faster).

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,9 @@
 export default {
     '**/*.ts': (filenames) => [
-        `pnpm build & ${filenames
-            .map(filename => `tsc ${filename} --target es2022 --noErrorTruncation --skipLibCheck --importsNotUsedAsValues error --noEmit --module esnext --moduleResolution node --allowSyntheticDefaultImports --strict`).join(' & ')}`,
-        `pnpm test -- related ${filenames.join(' ')}`
+        filenames
+            .map(filename => `bash -c "tsc ${filename} --target es2022 --noErrorTruncation --skipLibCheck --importsNotUsedAsValues error --noEmit --module esnext --moduleResolution node --allowSyntheticDefaultImports --strict"`)
+            .join(' & '),
+        `pnpm build`,
+        `vitest --run --no-threads related ${filenames.join(' ')}`
     ]
 }
