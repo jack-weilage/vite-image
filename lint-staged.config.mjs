a99ba2e9af2cd88d71c8b8ad4bdb9c4cc036f3a7
Jack Weilage <jack@weilage.dev>
2022-11-29 09:55:08 -0800

    Fix development for npm/Windows.

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,9 +1,10 @@
+const tsconfig = '--target es2022 --noErrorTruncation --skipLibCheck --importsNotUsedAsValues error --noEmit --module esnext --moduleResolution node --allowSyntheticDefaultImports --strict'
 export default {
-    '**/*.ts': (filenames) => [
-        filenames
-            .map(filename => `bash -c "tsc ${filename} --target es2022 --noErrorTruncation --skipLibCheck --importsNotUsedAsValues error --noEmit --module esnext --moduleResolution node --allowSyntheticDefaultImports --strict"`)
-            .join(' & '),
-        `pnpm build:code`,
+    '**/*.ts': (/** @type string[] */ filenames) => [
+        `concurrently ${filenames
+            .map(filename => `"tsc ${filename} ${tsconfig}"`)
+            .join(' ')}`,
+        `npm run build:code`,
         `vitest --run --no-threads related ${filenames.join(' ')}`
     ]
 }
