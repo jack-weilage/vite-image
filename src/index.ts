47968255d1d7c0e20b7c13956c67417b951e3c24
Jack Weilage <jack@weilage.dev>
2022-11-29 14:13:28 -0800

    Format/enforce code style.

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,12 @@ import { queue_transformers, copy_only_keys, create_configs, create_hash, dedupe
 import { createFilter, dataToEsm } from '@rollup/pluginutils'
 import MagicString from 'magic-string'
 import sharp from 'sharp'
-import { pathToFileURL } from 'url'
 
 export type { PluginConfig, Transformer, TypedImage } from '../types'
 
 /** The plugin, to be supplied to vite. */
-export default function image(user_plugin_config: Partial<PluginConfig> = {}): Plugin {
+export default function image(user_plugin_config: Partial<PluginConfig> = {}): Plugin
+{
     const plugin_config: PluginConfig = parse_plugin_config(user_plugin_config)
 
     const cache = new Map() as Cache
@@ -27,7 +27,8 @@ export default function image(user_plugin_config: Partial<PluginConfig> = {}): P
         // Called when the vite config is finalized.
         configResolved(config) { vite_config = config },
         // Called when a resource is being processed.
-        async load(id: string) {
+        async load(id: string)
+        {
             if (!filter(id))
                 return null
 
@@ -36,10 +37,10 @@ export default function image(user_plugin_config: Partial<PluginConfig> = {}): P
             // Deal with export tags here so it can be removed from url.
             const user_exports = searchParams.get('export')
                 ?.split(plugin_config.deliminator) as (keyof OutputImage)[] | undefined
-            
+
             const exports = dedupe(user_exports ?? (plugin_config.default_exports || []))
                 .filter(Boolean)
-            
+
             // Remove `export` from search params to prevent having to deal with it later.
             searchParams.delete('export')
 
@@ -49,15 +50,17 @@ export default function image(user_plugin_config: Partial<PluginConfig> = {}): P
                 .withMetadata()
                 .toBuffer())
 
-            const transformers = [ ...plugin_config.transformers, ...default_transformers ]
+            const transformers = [...plugin_config.transformers, ...default_transformers]
 
             const images = [] as InternalImage[]
-            for (const config of create_configs(searchParams, plugin_config.deliminator)) {
+            for (const config of create_configs(searchParams, plugin_config.deliminator))
+            {
                 // Create a unique hash based on the filename and config (prevents accidentally ingesting the same image twice).
                 const hash = create_hash(pathname + JSON.stringify(config))
 
                 // If we've already processed this exact image/config...
-                if (cache.has(hash)) {
+                if (cache.has(hash))
+                {
                     // Just grab what we already have from the digest.
                     images.push((cache.get(hash) as CacheEntry).data)
                     continue
@@ -68,7 +71,7 @@ export default function image(user_plugin_config: Partial<PluginConfig> = {}): P
                 // If the image didn't match a transformer, it shouldn't be processed
                 if (queued_transformers.length === 0)
                     continue
-                
+
                 // `apply_transformers` doesn't actually run the transformations, only queues them.
                 // We don't need the source in dev mode, but will always need the metadata.
                 const { info, data: source } = await image.toBuffer({ resolveWithObject: true })
@@ -76,7 +79,8 @@ export default function image(user_plugin_config: Partial<PluginConfig> = {}): P
                 // If we're in dev mode, we should supply an actual url here.
                 let src = DEV_PREFIX + hash
                 // If we're not in dev mode...
-                if (!this.meta.watchMode) {
+                if (!this.meta.watchMode)
+                {
                     const name = `${filename(pathname)}.${info.format}`
 
                     const handle = this.emitFile({ name, source, type: 'asset' })
@@ -107,7 +111,8 @@ export default function image(user_plugin_config: Partial<PluginConfig> = {}): P
         },
         //TODO: Add testing for dev mode (not 100% sure this works).
         // Called in dev/preview mode.
-        configureServer(server) {
+        configureServer(server)
+        {
             server.middlewares.use((req, res, next) => {
                 // If there's somehow no URL, this couldn't be an image.
                 if (!req.url)
@@ -116,13 +121,13 @@ export default function image(user_plugin_config: Partial<PluginConfig> = {}): P
                 // If the URL doesn't match the regex, this couldn't be in cache.
                 if (!DEV_REGEX.test(req.url))
                     return next()
-                
-                const [, hash ] = req.url.match(DEV_REGEX)!
-                
+
+                const [, hash] = req.url.match(DEV_REGEX)!
+
                 // If the image isn't in the cache, we can't do anything with it.
                 if (!cache.has(hash))
                     return next()
-                
+
                 const { image } = cache.get(hash) as CacheEntry
 
                 // Pipe the image to response.
@@ -132,11 +137,12 @@ export default function image(user_plugin_config: Partial<PluginConfig> = {}): P
         },
         //TODO: Write dedicated build tests.
         // Called in build mode.
-        renderChunk(code) {
+        renderChunk(code)
+        {
             // string.includes is quite a bit faster here.
             if (!code.includes(BUILD_PREFIX))
                 return null
-            
+
             const replacer = (_: string, hash: string) => vite_config.base + this.getFileName(hash)
 
             // If we don't need to generate a sourcemap, return early without using MagicString.
