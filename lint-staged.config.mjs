775ae79526953f71a64fb45c2ecaa727cc2f2c1e
Jack Weilage <jack@weilage.dev>
2022-11-29 18:01:12 -0800

    Run `ESLint` before committing.

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,10 +1,13 @@
 const tsconfig = '--target es2022 --noErrorTruncation --skipLibCheck --importsNotUsedAsValues error --noEmit --module esnext --moduleResolution node --allowSyntheticDefaultImports --strict'
+
 export default {
+    //eslint-disable-next-line no-inline-comments
     '**/*.ts': (/** @type string[] */ filenames) => [
+        `npm run lint ${filenames.join(' ')} --fix`,
         `concurrently ${filenames
             .map(filename => `"tsc ${filename} ${tsconfig}"`)
             .join(' ')}`,
-        `npm run build:code`,
+        'npm run build:code',
         `vitest --run --no-threads related ${filenames.join(' ')}`
     ]
 }
