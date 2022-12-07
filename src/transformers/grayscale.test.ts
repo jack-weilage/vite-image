b0f3cc128967ac5e9ed48319943218fd75950243
Jack Weilage <jack@weilage.dev>
2022-12-07 09:35:49 -0800

    Fix test.

--- src/transformers/grayscale.test.ts
+++ src/transformers/grayscale.test.ts
@@ -2,18 +2,18 @@ import { expect, it } from 'vitest'
 import { queue_transformers, create_hash } from '../utils'
 
 import { base_hash, base_image } from '../../tests/utils'
-import transformer from './tint'
+import transformer from './grayscale'
 
-it.each([ '#fff', '#1cd', '#face', '#ffaacc99' ])('applies the transform tint=%s', async (input) => {
-    const { image } = await queue_transformers(base_image.clone(), { tint: input }, [ transformer ])
+it.each([ true, false ])('applies the transform grayscale=%s', async (input) => {
+    const { image } = await queue_transformers(base_image.clone(), { grayscale: input }, [ transformer ])
 
     expect(create_hash(await image.toBuffer())).toMatchSnapshot()
 })
 
-it.each([ true, false, 'foo', 1, 100 ])('doesn\'t apply the transform tint=%s', async (input) => {
+it.each([ 'foo', 1, 100 ])('doesn\'t apply the transform grayscale=%s', async (input) => {
     const { image } = await queue_transformers(base_image.clone(), {
         //@ts-expect-error: Config shouldn't have these values.
-        tint: input
+        grayscale: input
     }, [ transformer ])
 
     expect(create_hash(await image.toBuffer())).toBe(base_hash)
