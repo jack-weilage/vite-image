fe884ce477bb1d5efcfe2335dd8520c69930ffc0
Jack Weilage <jack@weilage.dev>
2022-12-07 09:38:42 -0800

    Add a coverage reporter.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,16 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         testTimeout: process.env.CI ? 45000 : 15000,
-        include: [ '**/*.test.ts', '**/*.test-d.ts' ]
+        include: [ '**/*.test.ts', '**/*.test-d.ts' ],
+        coverage: {
+            enabled: true,
+            reporter: [ 'json-summary', 'text' ],
+            reportsDirectory: 'tests/coverage',
+            include: [ 'src/**/*' ],
+            exclude: [ 'src/transformers/index.ts' ],
+            // all: true,
+            skipFull: true,
+            perFile: true
+        }
     }
 })
