44729f1d55d810a4c57a747febefbb38d1ff78f6
jack-weilage <jack@weilage.dev>
2022-12-21 14:22:32 -0800

    `satisfies` is unnecessary here.

--- src/transformers/colorspace.ts
+++ src/transformers/colorspace.ts
@@ -1,5 +1,3 @@
-//TODO: Open a pull request in `@types/sharp`
-// `@types/sharp` thinks that `toColorspace` should take a string, but doesn't narrow any further than that.
 import type { Transformer } from '../../types'
 
 import { INPUT_COLORSPACES } from '../constants'
@@ -7,5 +5,5 @@ import { INPUT_COLORSPACES } from '../constants'
 export default {
     name: 'colorspace',
     matcher: (config) => INPUT_COLORSPACES.includes(config['colorspace']!),
-    transform: (img, config) => img.toColorspace(config['colorspace'] satisfies string)
+    transform: (img, config) => img.toColorspace(config['colorspace'])
 } satisfies Transformer<'colorspace'>
