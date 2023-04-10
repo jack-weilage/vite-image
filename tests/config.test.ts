dfb7f7aa81c778c8db2dbf1e279a0af1b817b186
jack-weilage <jack@weilage.dev>
2023-04-10 13:33:56 -0700

    Fix some flaky tests.

--- tests/config.test.ts
+++ tests/config.test.ts
@@ -7,17 +7,20 @@ test("doesn't apply an invalid config", () => {
 	assert.throws(
 		//@ts-expect-error: The config shouldn't have this key.
 		() => parse_plugin_config({ test: true }),
-		(err: unknown) => err instanceof AggregateError,
+		(err: unknown) => err instanceof Error,
+		'Unknown config key',
 	)
 	assert.throws(
 		//@ts-expect-error: The config shouldn't have this value.
 		() => parse_plugin_config({ include: 10 }),
-		(err: unknown) => err instanceof AggregateError,
+		(err: unknown) => err instanceof Error,
+		'Config values are of correct type (simple)',
 	)
 	assert.throws(
 		//@ts-expect-error: The config shouldn't have this value in an array.
 		() => parse_plugin_config({ transformers: ['foo', 'bar'] }),
-		(err: unknown) => err instanceof AggregateError,
+		(err: unknown) => err instanceof Error,
+		'Config values are of correct type (array)',
 	)
 })
 //TODO: Expand checks for valid config.
